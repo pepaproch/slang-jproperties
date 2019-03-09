2bdbf50e04bd3a8933208ba7175cc83811cc1c82
pepaproch@gmail.com <4erven8Linie>
2019-03-09 12:03:58 +0100

    add sonarcloud

--- travis.sh
+++ travis.sh
@@ -5,17 +5,23 @@ set -euo pipefail
   echo "Building slang"
   
   export INITIAL_VERSION=$(cat gradle.properties | grep version | awk -F= '{print $2}')
-  
-  ./gradlew --info --no-daemon --console plain \
-    -DbuildNumber=$TRAVIS_BUILD_NUMBER \
-     sonarqube \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN \
-    -Dsonar.projectVersion=$INITIAL_VERSION \
-    -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
-    -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
-    -Dsonar.analysis.sha1=$TRAVIS_COMMIT \
-    -Dsonar.organization=pepaproch-github
+
+  ./gradlew sonarqube \
+  -Dsonar.projectKey=pepaproch_slang-jproperties \
+  -Dsonar.organization=pepaproch-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=8b0bbe7510d0076ee15dde458cf327c7b13cfcf5
+
+#  ./gradlew --info --no-daemon --console plain \
+#    -DbuildNumber=$TRAVIS_BUILD_NUMBER \
+#     sonarqube \
+#    -Dsonar.host.url=$SONAR_HOST_URL \
+#    -Dsonar.login=$SONAR_TOKEN \
+#    -Dsonar.projectVersion=$INITIAL_VERSION \
+#    -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
+#    -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
+#    -Dsonar.analysis.sha1=$TRAVIS_COMMIT \
+#    -Dsonar.organization=pepaproch-github
 
 
 
