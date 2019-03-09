4194541c03243487f9ef6b286722e5916f7a2ab1
pepaproch@gmail.com <4erven8Linie>
2019-03-09 11:39:41 +0100

    add sonarcloud

--- travis.sh
+++ travis.sh
@@ -6,9 +6,9 @@ set -euo pipefail
   
   export INITIAL_VERSION=$(cat gradle.properties | grep version | awk -F= '{print $2}')
   
-  ./gradlew --info --no-daemon --console plain \
+  ./gradlew --debug --no-daemon --console plain \
     -DbuildNumber=$TRAVIS_BUILD_NUMBER \
-    build sonarqube \
+     sonarqube \
     -Dsonar.host.url=$SONAR_HOST_URL \
     -Dsonar.login=$SONAR_TOKEN \
     -Dsonar.projectVersion=$INITIAL_VERSION \
